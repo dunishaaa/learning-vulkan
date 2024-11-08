9c0fca4bd422da9af94dc1673a4ef3e33a6f761c
dunishaaa <aphimontes@outlook.com>
2024-11-07 21:48:19 -0600

    ayuda swapchain

---
+++ HelloTriangle/HelloTriangleApplication.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include <cstdint>
+#include <cstring>
+#include <vector>
+#include <vulkan/vk_platform.h>
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <optional>
+
+#include <cstdlib>
+
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+    const VkAllocationCallbacks *pAllocator,
+    VkDebugUtilsMessengerEXT *pDebugMessenger);
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+                                   VkDebugUtilsMessengerEXT debugMessenger,
+                                   const VkAllocationCallbacks *pAllocator);
+
+struct QueueFamilyIndices {
+  std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
+  bool isComplete() {
+    return graphicsFamily.has_value() && presentFamily.has_value();
+  }
+};
+
+struct SwapChainSupportDetails {
+  VkSurfaceCapabilitiesKHR capabilities;
+  std::vector<VkSurfaceFormatKHR> formats;
+  std::vector<VkPresentModeKHR> presentModes;
+};
+
+class HelloTriangleApplication {
+public:
+  void run();
+
+private:
+  VkQueue graphicsQueue;
+  VkQueue presentQueue;
+  VkInstance instance;
+  VkSurfaceKHR surface;
+
+  VkSwapchainKHR swapChain;
+  std::vector<VkImage> swapChainImages;
+  VkFormat swapChainImageFormat;
+  VkExtent2D swapChainExtent;
+
+  VkDebugUtilsMessengerEXT debugMessenger;
+  GLFWwindow *window;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  VkDevice device;
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL
+  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT messageType,
+                const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+                void *pUserData);
+  void initWindow();
+  bool checkValidationLayerSupport();
+  std::vector<const char *> getRequieredExtension();
+
+  void createInstance();
+
+  void initVulkan();
+
+  void createSurface();
+  void createLogicalDevice();
+  void createSwapChain();
+
+  bool isDeviceSuitable(VkPhysicalDevice device);
+  bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+  void pickPychiscalDevice();
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+  void populateDebugMessengerCreateInfo(
+      VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+  VkSurfaceFormatKHR chooseSwapSurfaceFormat(
+      const std::vector<VkSurfaceFormatKHR> &availableFormats);
+  VkPresentModeKHR chooseSwapPresentMode(
+      const std::vector<VkPresentModeKHR> &availablePresentModes);
+  VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
+  void setupDebugMessenger();
+  void mainLoop();
+  void cleanup();
+};
